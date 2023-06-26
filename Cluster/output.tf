2a2a297a59cbd97c6cc28ee6d1bb171c8d4eb662
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-06-26 17:12:35 +0200

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,12 +1,8 @@
-#output "public_ip_address" {
-#  value = azurerm_linux_virtual_machine..public_ip_address
-#}
-
-output "public_key" {
+output "pub" {
   value     = tls_private_key.SSH.public_key_pem
   sensitive = true
 }
 
-output "private_key" {
+output "private" {
   value = tls_private_key.SSH.private_key_pem
 }
